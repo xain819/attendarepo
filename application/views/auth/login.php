73b8f396db3cfb0a7711dc9f4555a727561a7cf4
HEART <raymundnilo1115@gmail.com>
2019-05-07 22:30:47 +0800

    change login title

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-          <title><?=isset($title)?$title:'Login - AdminLite' ?></title>
+          <title><?=isset($title)?$title:'Attenda Track' ?></title>
           <!-- Tell the browser to be responsive to screen width -->
           <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
           <!-- Bootstrap 3.3.6 -->
