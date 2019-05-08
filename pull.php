ef24645a778754d1f103e4fe5f1e117f0b4bb453
HEART <raymundnilo1115@gmail.com>
2019-05-08 20:49:38 +0800

    test

--- pull.php
+++ pull.php
@@ -1,9 +1,9 @@
 <?php
+    
+     // Use in the “Post-Receive URLs” section of your GitHub repo.
 
-// Use in the “Post-Receive URLs” section of your GitHub repo.
+      if ( $_POST['payload'] ) {
+      shell_exec( ‘cd /opt/bitnami/apache2/htdocs/ && git reset –hard HEAD && git pull’ );
+    }
 
-if ( $_POST['payload'] ) {
-shell_exec( ‘sudo cd /opt/bitnami/apache2/htdocs/ && git reset –hard HEAD && git pull’ );
-}
-
-?>hi
+?>
