8e699f810ce98e90696dfdef96f7b36cdd540988
jacobdenis <jacbolinodenis@gmail.com>
2019-05-16 01:35:40 +0800

    updated terminal

--- application/controllers/admin/Terminal.php
+++ application/controllers/admin/Terminal.php
@@ -8,12 +8,9 @@
 		}
 
 		public function index(){
-			$data['all_users'] = $this->dashboard_model->get_all_users();
-			$data['active_users'] = $this->dashboard_model->get_active_users();
-			$data['deactive_users'] = $this->dashboard_model->get_deactive_users();
-			$data['title'] = 'Dashboard';
-			$data['view'] = 'admin/generalsetting/index';
-			$this->load->view('layout', $data);
+			//$data['title'] = 'General Settings';
+			$data['view'] = 'terminal/terminal_modal';
+			$this->load->view('terminal/index',$data);
 		}
 		
 	}
