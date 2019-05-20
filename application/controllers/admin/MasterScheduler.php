6b9012982639c1433319cb3c9c61aa0973390c7d
HEART <raymundnilo1115@gmail.com>
2019-05-20 21:53:45 +0800

    add masterscheduler

--- application/controllers/admin/MasterScheduler.php
+++ application/controllers/admin/MasterScheduler.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-	class Masterscheduler extends MY_Controller {
+	class MasterScheduler extends MY_Controller {
 		public function __construct(){
 			parent::__construct();
 			$this->load->library('rbac');
