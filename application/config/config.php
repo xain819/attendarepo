92a0332d75367a0f155e58b8af6e3c87499d47d8
HEART <raymundnilo1115@gmail.com>
2019-05-20 21:29:24 +0800

    change case

--- application/config/config.php
+++ application/config/config.php
@@ -26,8 +26,8 @@ ob_start();
 */
 
 $url = "http://".$_SERVER['HTTP_HOST']. str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-//$config['base_url'] = 'http://35.193.161.159/';
-$config['base_url'] = 'http://localhost/';
+$config['base_url'] = 'http://35.193.161.159/';
+//$config['base_url'] = 'http://localhost/';
 
 
 
