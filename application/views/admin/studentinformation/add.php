d412528dd06679ce571df22cc160530a579f33b2
HEART <raymundnilo1115@gmail.com>
2019-06-25 09:34:02 +0800

    added academic settings

--- application/views/admin/studentinformation/add.php
+++ application/views/admin/studentinformation/add.php
@@ -1,4 +1,4 @@
-<div class="modal modal-default fade" id="student-modal-primary">
+<div class="modal modal-default " id="student-modal-primary">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
               <div class="modal-header">
