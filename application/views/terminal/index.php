59f9410c2b2c63b304f9d0eb0a5f18e3a991cc7d
raymund <raymundnilo1115@gmail.com>
2019-09-02 23:55:03 -0500

    fix font size of terminal

--- application/views/terminal/index.php
+++ application/views/terminal/index.php
@@ -599,78 +599,78 @@ $(document).ready(function(){
   
 
 
-  var Clock = (function(){
+  // var Clock = (function(){
 
-  var exports = function(element) {
-    this._element = element;
-    var html = '';
-    for (var i=0;i<6;i++) {
-      html += '<span>&nbsp;</span>';
-    }
-    this._element.innerHTML = html;
-    this._slots = this._element.getElementsByTagName('span');
-    this._tick();
-  };
+  // var exports = function(element) {
+  //   this._element = element;
+  //   var html = '';
+  //   for (var i=0;i<6;i++) {
+  //     html += '<span>&nbsp;</span>';
+  //   }
+  //   this._element.innerHTML = html;
+  //   this._slots = this._element.getElementsByTagName('span');
+  //   this._tick();
+  // };
 
-  exports.prototype = {
+  // exports.prototype = {
 
-    _tick:function() {
-      var time = new Date();
-      this._update(this._pad(time.getHours()) + this._pad(time.getMinutes()) + this._pad(time.getSeconds()));
-      var self = this;
-      setTimeout(function(){
-        self._tick();
-      },1000);
-    },
+  //   _tick:function() {
+  //     var time = new Date();
+  //     this._update(this._pad(time.getHours()) + this._pad(time.getMinutes()) + this._pad(time.getSeconds()));
+  //     var self = this;
+  //     setTimeout(function(){
+  //       self._tick();
+  //     },1000);
+  //   },
 
-    _pad:function(value) {
-      return ('0' + value).slice(-2);
-    },
+  //   _pad:function(value) {
+  //     return ('0' + value).slice(-2);
+  //   },
 
-    _update:function(timeString) {
+  //   _update:function(timeString) {
 
-      var i=0,l=this._slots.length,value,slot,now;
-      for (;i<l;i++) {
+  //     var i=0,l=this._slots.length,value,slot,now;
+  //     for (;i<l;i++) {
 
-        value = timeString.charAt(i);
-        slot = this._slots[i];
-        now = slot.dataset.now;
+  //       value = timeString.charAt(i);
+  //       slot = this._slots[i];
+  //       now = slot.dataset.now;
 
-        if (!now) {
-          slot.dataset.now = value;
-          slot.dataset.old = value;
-          continue;
-        }
+  //       if (!now) {
+  //         slot.dataset.now = value;
+  //         slot.dataset.old = value;
+  //         continue;
+  //       }
 
-        if (now !== value) {
-          this._flip(slot,value);
-        }
-      }
-    },
+  //       if (now !== value) {
+  //         this._flip(slot,value);
+  //       }
+  //     }
+  //   },
 
-    _flip:function(slot,value) {
+  //   _flip:function(slot,value) {
 
-      // setup new state
-      slot.classList.remove('flip');
-      slot.dataset.old = slot.dataset.now;
-      slot.dataset.now = value;
+  //     // setup new state
+  //     slot.classList.remove('flip');
+  //     slot.dataset.old = slot.dataset.now;
+  //     slot.dataset.now = value;
 
-      // force dom reflow
-      slot.offsetLeft;
+  //     // force dom reflow
+  //     slot.offsetLeft;
 
-      // start flippin
-      slot.classList.add('flip');
+  //     // start flippin
+  //     slot.classList.add('flip');
 
-    }
+  //   }
 
-  };
+  // };
 
-  return exports;
-  }());
+  // return exports;
+  // }());
 
-  var i=0,clocks = document.querySelectorAll('.clock'),l=clocks.length;
-  for (;i<l;i++) {
-  new Clock(clocks[i]);
+  // var i=0,clocks = document.querySelectorAll('.clock'),l=clocks.length;
+  // for (;i<l;i++) {
+  // new Clock(clocks[i]);
   }
 })
 </script>
