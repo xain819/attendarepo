9df85a31decf6686e73895c458189db2c30cb7ce
KuganSemion <kugansemion@gmail.com>
2019-12-17 21:46:19 +0800

    System Setting
    Updated added manage system setting

---
+++ application/views/admin/academicsettings/edit-systemsetting.php
@@ -0,0 +1,28 @@
+<div class="modal fade modal-default" id="edit-systemsetting-modal-primary">
+    <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">System Settings</h4>
+        </div>
+        <div class="modal-body mydetails">
+            <form>
+                <div class="form-group">
+                    <label class="text-label">SettingName</label>
+                    <input type="text" id="settingname-edit" name="text" class="form-control" " required="">
+                </div>
+                <div class="form-group">
+                    <label class="text-label">Setting Value</label>
+                    <input type="text" id="settingvalue-edit" name="text" class="form-control"  required="">
+                </div>
+                
+                <button type="button"  id="edit-systemsetting-btn" class="btn btn-primary ">Submit</button>
+                <button type="button" class="btn btn-light text-dark">Cancel</button>
+            </form>
+        </div>
+    </div>
+    <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
