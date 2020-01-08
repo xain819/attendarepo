5056811aec5fde3a12868b00fbc496b09e38cbeb
kugansemion <52968503+kugansemion@users.noreply.github.com>
2020-01-08 20:19:47 +0800

    abc
    abc

--- application/views/admin/admin/settings.php
+++ application/views/admin/admin/settings.php
@@ -1,5 +1,5 @@
 <style>
-button.btn-space {
+  button.btn-space {  
         margin-left: 1em;
     }
 </style>
@@ -388,6 +388,32 @@ button.btn-space {
                                             </div>
                                         </div>
                                     </li>
+                                    <li><a href="#step-5">05</a>
+                                        <div class="form-wizard-content">
+                                            <div class="form-wizard-content-upper">
+                                                <img src="../../assets/images/avatar/10.png" alt="">
+                                                <h4>Import Class </h4>
+                                                <p>Upload and Import Master SIS data to AttendaTrack</p>
+                                            </div>
+                                            <div class="form-wizard-content-bottom">
+                                                <h4>(+1)408-657-9007</h4>
+                                                <a href="mailto:support@pocketprincipal.com">support@pocketprincipal.com</a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li><a href="#step-6">06</a>
+                                        <div class="form-wizard-content">
+                                            <div class="form-wizard-content-upper">
+                                                <img src="../../assets/images/avatar/10.png" alt="">
+                                                <h4>Import Class </h4>
+                                                <p>Upload and Import Master SIS data to AttendaTrack</p>
+                                            </div>
+                                            <div class="form-wizard-content-bottom">
+                                                <h4>(+1)408-657-9007</h4>
+                                                <a href="mailto:support@pocketprincipal.com">support@pocketprincipal.com</a>
+                                            </div>
+                                        </div>
+                                    </li>
                                 </ul>
                                 <!-- </div> -->
                                 <div id="abc" class="form-wizard-right">
@@ -545,6 +571,64 @@ button.btn-space {
                                                 
                                         </table>
                                     </div>
+                                    <div id="step-5" class="">
+                                      <div class=" card box-body" style='padding:20px;'>
+                                      <div class=' card col-xl-12 '>
+                                      <table id="classes" class="display" style="width:100%">
+                                              <thead>
+                                                  <tr>
+
+                                                        
+                                          
+                                                
+                                                  <th>Class Code</th>
+                                                      <th>Course Code</th>
+                                                      <th>Type</th>
+                                                      <th>Term</th>
+                                                      <th>Day</th>
+                                                      <th>Period</th>
+                                                      <th>Level</th>
+                                                      <th>Section</th>
+                                                      <th>Location</th>
+                                                      <th>TeacherID</th>
+                                                      <th>StudentID</th>
+                                                    
+                                              
+                                                        
+                                                  
+                                                  </tr>
+                                              </thead>
+                                        
+                                          </table></div>
+                                        </div>
+
+                                    </div>
+                                    <div id="step-6" class="">
+                                    <div class="card box-body" style='padding:20px;'>
+
+                                    <table id="example" class="display" style="width:100%">
+                                            <thead>
+                                                <tr>
+                                        
+                                                    <th>ID</th>
+                                                    <th>COURSE GROUP</th>
+                                                    <th>COURSE CODE</th>
+
+                                                    <th>SHORT DESCRIPTION</th>
+                                                    <th>COURSE DESCRIPTION</th>
+                                                    <th>GRADE LEVEL</th>
+                                                    <th>CREDITS</th>
+                                                    <th>CREDITS</th>
+                                                
+                                          
+                                                
+                                                </tr>
+                                            </thead>
+                                      
+                                        </table>
+                                    </div> 
+
+                                    </div>
                                     <!-- // -->
                                 </div>
                             </div>
@@ -587,15 +671,406 @@ button.btn-space {
 
 <script src="https://cdn.datatables.net/select/1.3.0/js/dataTables.select.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script> -->
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+
+
 
 <script src="<?php echo base_url('public/dist/js/sweetalert.min.js');?>"></script>
     <script src="<?=base_url() ?>public/assets/plugins/smartwizard/dist/js/jquery.smartWizard.min.js"></script>
     <script src="<?=base_url() ?>public/js/plugins-init/smartwizard-init.js"></script>
+
+<!-- /////////COURSE/////////// -->
+<script type='text/javascript'>
+
+ var csrfName = '<?php echo $this->security->get_csrf_token_name(); ?>',
+        csrfHash = '<?php echo $this->security->get_csrf_hash(); ?>',
+        base_url='<?php echo base_url(); ?>';
+var editor;
+
+function selectColumns ( editor, csv, header ) {
+    var selectEditor = new $.fn.dataTable.Editor();
+    var fields = editor.order();
+    console.log(selectEditor);
+ 
+    for ( var i=0 ; i<fields.length ; i++ ) {
+        var field = editor.field( fields[i] );
+        
+ 
+        selectEditor.add( {
+            label: field.label(),
+            name: field.name(),
+            type: 'select',
+            options: header,
+            def: header[i]
+        } );
+    
+    }
+ 
+    selectEditor.create({
+        title: 'Map CSV fields',
+        buttons: 'Import '+csv.length+' records',
+        message: 'Select the CSV column you want to use the data from for each field.'
+    });
+ 
+    selectEditor.on('submitComplete', function (e, json, data, action) {
+        // Use the host Editor instance to show a multi-row create form allowing the user to submit the data.
+        editor.create( csv.length, {
+            title: 'Confirm import',
+            buttons: 'Submit',
+            message: 'Click the <i>Submit</i> button to confirm the import of '+csv.length+' rows of data. Optionally, override the value for a field to set a common value by clicking on the field below.'
+        } );
+ 
+        for ( var i=0 ; i<fields.length ; i++ ) {
+            var field = editor.field( fields[i] );
+            var mapped = data[ field.name() ];
+ 
+            for ( var j=0 ; j<csv.length ; j++ ) {
+                field.multiSet( j, csv[j][mapped] );
+            }
+        }
+     //   a.ajax.reload();
+    } );
+}
+
+ 
+$(document).ready(function() {
+    // Regular editor for the table
+    base_url='<?php echo base_url(); ?>';
+    editor = new $.fn.dataTable.Editor( {
+        ajax: {
+            url: base_url+"admin/academicsettings/check_courses",
+            data:({ [csrfName]: csrfHash}),
+            type:"POST",
+            dataSrc: '',
+            dataType:'JSON'
+       },
+      
+        table: "#example",
+        fields: [ 
+            {
+                label: "ID:",
+                name: "id"
+            },
+            {
+                label: "Course Group:",
+                name: "course_group"
+            },
+            {
+                label: "Course Code:",
+                name: "course_code"
+            },
+            {
+                label: "Short Description:",
+                name: "short_desc"
+            },
+            {
+                label: "Course Description:",
+                name: "course_description"
+            },
+            {
+                label: "Grade Leve:",
+                name: "teacher_name"
+            },
+            {
+                label: "Grade Level:",
+                name: "grade_level"
+            },
+            {
+                label: "Credits:",
+                name: "credits"
+            }
+        
+
+        ]
+        
+    } );
+ 
+    //lumalabas nman na kaso may error na 403
+    //not allowed daw try ko sir.mag import felling ko sa 
+    var a= $('#example').DataTable( {
+        dom: 'Bfrtip',
+        ajax: {
+            url: base_url+"admin/academicsettings/get_import_courses",
+            data:({ [csrfName]: csrfHash}),
+            type:"POST",
+            dataSrc: '',
+            dataType:'JSON'
+       },
+        columns: [
+            { data: 'id' },
+            { data: 'course_group' },
+            { data: 'course_code' },
+            { data: 'short_desc' },
+
+            { data: 'course_description' },
+            { data: 'grade_level' },
+            { data: 'credits' },
+            { data:null},
+            
+            
+         
+
+          
+        ],
+        select: true,
+        buttons: [
+            { extend: 'create', editor: editor },
+            { extend: 'edit',   editor: editor },
+            { extend: 'remove', editor: editor },
+            {
+                extend: 'csv',
+                text: 'Export CSV',
+                className: 'btn-space',
+                exportOptions: {
+                    orthogonal: null
+                }
+            },
+            {
+                text: 'Import CSV',
+                action: function () {
+                    uploadEditor.create( {
+                        title: 'CSV file import'
+                    } );
+                }
+            },
+            {
+                extend: 'selectAll',
+                className: 'btn-space'
+            },
+            'selectNone',
+        ]
+    });
+    // Upload Editor - triggered from the import button. Used only for uploading a file to the browser
+    var uploadEditor = new $.fn.dataTable.Editor( {
+        fields: [ {
+            label: 'CSV file:',
+            name: 'csv',
+            type: 'upload',
+            ajax: function ( files ) {
+                // Ajax override of the upload so we can handle the file locally. Here we use Papa
+                // to parse the CSV.
+                Papa.parse(files[0], {
+                    header: true,
+                    skipEmptyLines: true,
+                    complete: function (results) {
+                        if ( results.errors.length ) {
+                            console.log( results );
+                            uploadEditor.field('csv').error( 'CSV parsing error: '+ results.errors[0].message );
+                        }
+                        else {
+                            uploadEditor.close();
+                          
+                            selectColumns( editor, results.data, results.meta.fields );
+                            console.log( results.data[0].Firstname );
+                            
+                          
+                        }
+                    }
+                });
+            }
+        } ]
+    } );
+    editor.on('postSubmit', function(event, data, action) {
+        
+     a.ajax.reload();
+    })
+    editor.on( 'create', function ( e, json, data ) {
+    alert( 'New row added' );
+} );
+    
+});
+</script>
+
+<!-- /////////Classes//////// -->
+    <script type='text/javascript'>
+ var csrfName = '<?php echo $this->security->get_csrf_token_name(); ?>',
+        csrfHash = '<?php echo $this->security->get_csrf_hash(); ?>',
+        base_url='<?php echo base_url(); ?>';
+        
+var editor;
+
+function selectColumns ( editor, csv, header ) {
+    var selectEditor = new $.fn.dataTable.Editor();
+    var fields = editor.order();
+    console.log(selectEditor);
+ 
+    for ( var i=0 ; i<fields.length ; i++ ) {
+        var field = editor.field( fields[i] );
+        
+ 
+        selectEditor.add( {
+            label: field.label(),
+            name: field.name(),
+            type: 'select',
+            options: header,
+            def: header[i]
+        } );
+    
+    }
+ 
+    selectEditor.create({
+        title: 'Map CSV fields',
+        buttons: 'Import '+csv.length+' records',
+        message: 'Select the CSV column you want to use the data from for each field.'
+    });
+ 
+    selectEditor.on('submitComplete', function (e, json, data, action) {
+        // Use the host Editor instance to show a multi-row create form allowing the user to submit the data.
+        editor.create( csv.length, {
+            title: 'Confirm import',
+            buttons: 'Submit',
+            message: 'Click the <i>Submit</i> button to confirm the import of '+csv.length+' rows of data. Optionally, override the value for a field to set a common value by clicking on the field below.'
+        } );
+ 
+        for ( var i=0 ; i<fields.length ; i++ ) {
+            var field = editor.field( fields[i] );
+            var mapped = data[ field.name() ];
+ 
+            for ( var j=0 ; j<csv.length ; j++ ) {
+                field.multiSet( j, csv[j][mapped] );
+            }
+        }
+     //   a.ajax.reload();
+    } );
+}
+
+
+ 
+$(document).ready(function() {
+    // Regular editor for the table
+    editor = new $.fn.dataTable.Editor( {
+        ajax: {
+            url: base_url+"admin/academicsettings/check_classes",
+            data:({ [csrfName]: csrfHash}),
+            type:"POST",
+            dataSrc: '',
+            dataType:'JSON'
+       },
+       
+        table: "#classes",
+        fields: [ 
+          
+            { label: "Class Code:",name: "class_code" },
+            { label: "Course Code:",name: "course_code" },
+            { label: "Class Type:",name: "class_type" },
+            { label: "Term:",name: "term" },
+            { label: "Schedule Type:",name: "schedule_type" },
+            { label: "Period:",name: "period_number" },
+            { label: "Grade Level:",name: "grade_level" },
+            { label: "Section:",name: "section" },
+            { label: "Location:",name: "location" },
+            { label: "Teacher ID:",name: "teacher_id_number" },
+            { label: "State ID:",name: "state_uid" },
+            { label: "Student Local ID:",name: "student_local_id" },
+
+            
+        ]
+        
+    } );
+ 
+    //lumalabas nman na kaso may error na 403
+    //not allowed daw try ko sir.mag import felling ko sa 
+    var a= $('#classes').DataTable( {
+        dom: 'Bfrtip',
+        ajax: {
+            url: base_url+"admin/academicsettings/get_import_classes",
+            data:({ [csrfName]: csrfHash}),
+            type:"POST",
+            dataSrc: '',
+            dataType:'JSON'
+       },
+        columns: [
+        
+           
+            { data: 'class_code' },
+            { data: 'course_code' },
+            { data: 'class_type' },
+            { data: 'term' },
+            { data: 'schedule_type' },
+            { data: 'period_number' },
+            { data: 'grade_level' },
+            { data: 'section' },
+            { data: 'location' },
+            { data: 'teacher_id_number' },
+
+            { data: 'student_local_id' },
+           
+        
+
+          
+        ],
+        select: true,
+        buttons: [
+            { extend: 'create', editor: editor },
+            { extend: 'edit',   editor: editor },
+            { extend: 'remove', editor: editor },
+            {
+                extend: 'csv',
+                text: 'Export CSV',
+                className: 'btn-space',
+                exportOptions: {
+                    orthogonal: null
+                }
+            },
+            {
+                text: 'Import CSV',
+                action: function () {
+                    uploadEditor.create( {
+                        title: 'CSV file import'
+                    } );
+                }
+            },
+            {
+                extend: 'selectAll',
+                className: 'btn-space'
+            },
+            'selectNone',
+        ]
+    });
+    // Upload Editor - triggered from the import button. Used only for uploading a file to the browser
+    var uploadEditor = new $.fn.dataTable.Editor( {
+        fields: [ {
+            label: 'CSV file:',
+            name: 'csv',
+            type: 'upload',
+            ajax: function ( files ) {
+                // Ajax override of the upload so we can handle the file locally. Here we use Papa
+                // to parse the CSV.
+                Papa.parse(files[0], {
+                    header: true,
+                    skipEmptyLines: true,
+                    complete: function (results) {
+                        if ( results.errors.length ) {
+                            console.log( results );
+                            uploadEditor.field('csv').error( 'CSV parsing error: '+ results.errors[0].message );
+                        }
+                        else {
+                            uploadEditor.close();
+                          
+                            selectColumns( editor, results.data, results.meta.fields );
+                            console.log( results.data[0].Firstname );
+                            
+                          
+                        }
+                    }
+                });
+            }
+        } ]
+    } );
+    editor.on('postSubmit', function(event, data, action) {
+        
+     a.ajax.reload();
+    })
+    editor.on( 'create', function ( e, json, data ) {
+    alert( 'New row added' );
+} );
+});
+</script>
+
 
     <script>
    
