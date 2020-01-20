5264ce0541c1b5c14a3b6f8c68358c94c14f9eb7
raymund07 <raymundnilo1115@gmail.com>
2020-01-20 10:52:45 +0800

    push to server

--- application/views/admin/admin/list.php
+++ application/views/admin/admin/list.php
@@ -19,11 +19,11 @@
             <tr>
             
                 <th>User</th>
-                <th width="150" >Username or ID</th>
+                <th width="100" >Username or ID</th>
                 <th>Email</th>
                 <th>Role</th>
-         
-                <th width="100">Status</th>
+                <th width="100">Active</th>
+   
                 <th width="120">Action</th>
             </tr>
         </thead>
@@ -50,25 +50,19 @@
                 <td>
                     <button class="btn btn-xs btn-success"><?=$row['admin_role_title']?></button>
                 </td> 
-          
+                <td><input class='tgl tgl-ios tgl_checkbox' 
+                    data-id="<?=$row['admin_id']?>" 
+                    id='cb_<?=$row['admin_id']?>' 
+                    type='checkbox' <?php echo ($row['is_active'] == 1)? "checked" : ""; ?> />
+                    <label class='tgl-btn' for='cb_<?=$row['admin_id']?>'></label>
+                </td>
 
-                <td>
-                    <?=$row_status
-                    
-                    
-                    
-                    ?>
-                </td> 
+           
                 <td>
                     <a href="<?php echo site_url("admin/admin/edit/".$row['admin_id']); ?>" class="btn btn-warning btn-xs mr5" >
                     <i class="fa fa-edit"></i>
                     </a>
                     <a href="<?php echo site_url("admin/admin/delete/".$row['admin_id']); ?>" onclick="return confirm('are you sure to delete?')" class="btn btn-danger btn-xs"><i class="fa fa-remove"></i></a>
-                    <input class='tgl tgl-ios tgl_checkbox' 
-                    data-id="<?=$row['admin_id']?>" 
-                    id='cb_<?=$row['admin_id']?>' 
-                    type='checkbox' <?php echo ($row['is_active'] == 1)? "checked" : ""; ?> />
-                    <label class='tgl-btn' for='cb_<?=$row['admin_id']?>'></label>
                 </td>
             </tr>
             <?php endforeach;?>
