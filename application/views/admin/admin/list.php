62c3a69897b8c9b354e221607c91781ef4487b7f
raymund07 <raymundnilo1115@gmail.com>
2020-01-20 10:48:58 +0800

    push to server

--- application/views/admin/admin/list.php
+++ application/views/admin/admin/list.php
@@ -17,21 +17,26 @@
     <table id="example1" class="table table-bordered table-hover">
         <thead>
             <tr>
-                <th width="50">ID</th>
+            
                 <th>User</th>
-                <th>User Name</th>
+                <th width="150" >Username or ID</th>
                 <th>Email</th>
                 <th>Role</th>
+         
                 <th width="100">Status</th>
                 <th width="120">Action</th>
             </tr>
         </thead>
         <tbody>
-            <?php foreach($info as $row): ?>
+            <?php foreach($info as $row):
+                 if($row['is_active']==='1'){
+                    $row_status='active';
+                 }
+                 else{ $row_status='in-active';}
+            ?>
+    
             <tr>
-            	<td>
-					<?=$row['admin_id']?>
-                </td>
+            
                 <td>
 					<h4 class="m0 mb5"><?=$row['firstname']?> <?=$row['lastname']?></h4>
                     <small class="text-muted"><?=$row['admin_role_title']?></small>
@@ -45,17 +50,25 @@
                 <td>
                     <button class="btn btn-xs btn-success"><?=$row['admin_role_title']?></button>
                 </td> 
-                <td><input class='tgl tgl-ios tgl_checkbox' 
-                    data-id="<?=$row['admin_id']?>" 
-                    id='cb_<?=$row['admin_id']?>' 
-                    type='checkbox' <?php echo ($row['is_active'] == 1)? "checked" : ""; ?> />
-                    <label class='tgl-btn' for='cb_<?=$row['admin_id']?>'></label>
-                </td>
+          
+
+                <td>
+                    <?=$row_status
+                    
+                    
+                    
+                    ?>
+                </td> 
                 <td>
                     <a href="<?php echo site_url("admin/admin/edit/".$row['admin_id']); ?>" class="btn btn-warning btn-xs mr5" >
                     <i class="fa fa-edit"></i>
                     </a>
                     <a href="<?php echo site_url("admin/admin/delete/".$row['admin_id']); ?>" onclick="return confirm('are you sure to delete?')" class="btn btn-danger btn-xs"><i class="fa fa-remove"></i></a>
+                    <input class='tgl tgl-ios tgl_checkbox' 
+                    data-id="<?=$row['admin_id']?>" 
+                    id='cb_<?=$row['admin_id']?>' 
+                    type='checkbox' <?php echo ($row['is_active'] == 1)? "checked" : ""; ?> />
+                    <label class='tgl-btn' for='cb_<?=$row['admin_id']?>'></label>
                 </td>
             </tr>
             <?php endforeach;?>
